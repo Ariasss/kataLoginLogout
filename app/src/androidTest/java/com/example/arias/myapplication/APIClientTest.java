7150d4b0745c448e6abd4d594d9ba1ead54379c7
Arias <jarias@apperian.com>
2017-07-26 16:48:04 +0200

    Fix

--- app/src/androidTest/java/com/example/arias/myapplication/APIClientTest.java
+++ app/src/androidTest/java/com/example/arias/myapplication/APIClientTest.java
@@ -14,19 +14,27 @@ public class APIClientTest {
     }
 
     @Test
-    public void logout() throws Exception {
-        APIClient apiClient = new APIClient(new Clock());
+    public void logoutOK() throws Exception {
+        APIClient apiClient = new APIClient(new MockClock(1000));
         boolean res = apiClient.logout();
         assertEquals(res,true);
     }
 
-
-
-    class MockClock extends Clock{
-    @Override
-        public long getCurrentTime(){
-            return 1000;
-        }
+    @Test
+    public void logoutFail() throws Exception {
+        APIClient apiClient = new APIClient(new MockClock(1001));
+        boolean res = apiClient.logout();
+        assertEquals(res,false);
     }
+}
 
+class MockClock extends Clock{
+    private int fixedTime;
+    MockClock(int timeToSend){
+        fixedTime = timeToSend;
+    }
+    @Override
+    public long getCurrentTime(){
+        return 1000;
+    }
 }
